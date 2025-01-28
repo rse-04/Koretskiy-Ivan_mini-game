9b63216fec355ee882e857d266fd08e781bd727a
rse-04 <125733188+rse-04@users.noreply.github.com>
2025-01-28 20:00:52 +0300

    Add files via upload

---
+++ Иван_Программа_проекта.ino
@@ -0,0 +1,17 @@
+void zaryad(){}
+void smszaryad(){}
+void ishod_igr(){}
+void vistrel(){}
+void down(){}
+void up(){}
+
+/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
